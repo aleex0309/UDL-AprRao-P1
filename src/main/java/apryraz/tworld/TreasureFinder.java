69fe8d9bc79a72db6cda9c9e416606666b3a8e21
marcgj <mgaspjoval2002@gmail.com>
2023-04-22 19:40:23 +0200

    inferenceQuestions done

--- src/main/java/apryraz/tworld/TreasureFinder.java
+++ src/main/java/apryraz/tworld/TreasureFinder.java
@@ -370,24 +370,29 @@ public class TreasureFinder {
      **/
     public void performInferenceQuestions() throws IOException,
             ContradictionException, TimeoutException {
-        // EXAMPLE code to check this for position (2,3):
-        // Get variable number for position 2,3 in past variables
-        int linealIndex = coordToLineal(2, 3, TreasureFutureOffset);
-        // Get the same variable, but in the past subset
-        int linealIndexPast = coordToLineal(2, 3, TreasurePastOffset);
+        // For each position in the world do
+        for (int x = 1; x <= WorldDim; x++) {
+            for (int y = 1; y <= WorldDim; y++) {
+                // EXAMPLE code to check this for position (2,3):
+                // Get variable number for position 2,3 in past variables
+                int linealIndex = coordToLineal(x, y, TreasureFutureOffset);
+                // Get the same variable, but in the past subset
+                int linealIndexPast = coordToLineal(x, y, TreasurePastOffset);
 
-        VecInt variablePositive = new VecInt();
-        variablePositive.insertFirst(linealIndex);
+                VecInt variablePositive = new VecInt();
+                variablePositive.insertFirst(linealIndex);
 
-        // Check if Gamma + variablePositive is unsatisfiable:
-        // This is only AN EXAMPLE for a specific position: (2,3)
-        if (!(solver.isSatisfiable(variablePositive))) {
-            // Add conclusion to list, but rewritten with respect to "past" variables
-            VecInt concPast = new VecInt();
-            concPast.insertFirst(-(linealIndexPast));
+                // Check if Gamma + variablePositive is unsatisfiable:
+                // This is only AN EXAMPLE for a specific position: (2,3)
+                if (!(solver.isSatisfiable(variablePositive))) {
+                    // Add conclusion to list, but rewritten with respect to "past" variables
+                    VecInt concPast = new VecInt();
+                    concPast.insertFirst(-(linealIndexPast));
 
-            futureToPast.add(concPast);
-            tfstate.set(2, 3, "X");
+                    futureToPast.add(concPast);
+                    tfstate.set(x, y, "X");
+                }
+            }
         }
 
     }
